9b32a682921286ceef913e6e85ad3099b62ece19
Kyle Krull <kyle.krull@here.com>
2018-08-24 15:52:56 -0500

    Work from the base directory

--- asp-config/Cli/Program.cs
+++ asp-config/Cli/Program.cs
@@ -8,12 +8,34 @@ namespace Cli
   {
     static void Main(string[] args)
     {
+      var assemblyPath = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+      var sourcePath = ClosestAncestorWithFile(assemblyPath.Parent, "appsettings.json");
+
       var builder = new ConfigurationBuilder()
-        .SetBasePath(Directory.GetCurrentDirectory())
+        .SetBasePath(sourcePath.FullName)
         .AddJsonFile("appsettings.json");
 
       var configuration = builder.Build();
       Console.WriteLine($"{configuration["source"]}");
+    }
+
+    private static DirectoryInfo ClosestAncestorWithFile(DirectoryInfo initialDirectory, string globPattern)
+    {
+      for (var current = initialDirectory; current != null; current = current?.Parent)
+      {
+        var matchingFiles = current.GetFiles(globPattern);
+        if (matchingFiles.Length == 1)
+          return current;
+
+        if (matchingFiles.Length > 1)
+        {
+          throw new ArgumentException(
+            $"Ancestor {current} of {initialDirectory} contains multiple files matching {globPattern}");
+        }
+      }
+
+      throw new ArgumentException(
+        $"No ancestor of {initialDirectory.FullName} contains a single file matching {globPattern}");
     }
   }
 }
--- asp-config/run-tests.py
+++ asp-config/run-tests.py
@@ -8,18 +8,39 @@ cli_dir = os.path.join(base_dir, 'Cli')
 
 
 def main():
-    os.chdir(base_dir)
     run_cli_from_cli()
+    run_cli_from_base()
 
 
 def run_cli_from_cli():
-    os.chdir(cli_dir)
-    output = subprocess.check_output(['dotnet', 'run'])
-    assert_equal('Cli/appsettings.json', output.strip())
+    with cd(cli_dir):
+        output = subprocess.check_output(['dotnet', 'run'])
+        assert_equal('Cli/appsettings.json', output.strip())
+
+
+def run_cli_from_base():
+    with cd(base_dir):
+        output = subprocess.check_output(['dotnet', 'run', '--project', 'Cli'])
+        assert_equal('Cli/appsettings.json', output.strip())
 
 
 def assert_equal(expected, actual):
     assert expected == actual, 'Incorrect output\nexpect: <{}>\nactual: <{}>'.format(expected, actual)
+
+
+class cd:
+    """Context manager for changing the current working directory"""
+    def __init__(self, target_path):
+        self.target_path = os.path.expanduser(target_path)
+
+    def __enter__(self):
+        self.original_path = os.getcwd()
+        os.chdir(self.target_path)
+        print('> {}'.format(self.target_path))
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        os.chdir(self.original_path)
+        print('< {}'.format(self.target_path))
 
 
 if __name__ == '__main__':
