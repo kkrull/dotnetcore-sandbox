e59ff0c1934021e9c14a4d05f5a78103954acb42
Kyle Krull <kkrull@8thlight.com>
2019-06-04 12:42:54 -0500

    Attempt configuration with a local YAML file

--- SplitIO.Local/SplitIO.CLI/Program.cs
+++ SplitIO.Local/SplitIO.CLI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Splitio.Services.Client.Classes;
 
 namespace SplitIO.CLI
 {
@@ -6,7 +7,15 @@ namespace SplitIO.CLI
   {
     public static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Console.WriteLine($"Arguments[{args.Length}]:");
+      var localhostFilePath = args[0];
+      Console.WriteLine($"- localhostFilePath: {localhostFilePath}");
+
+      var config = new ConfigurationOptions();
+      config.Ready = 10000;
+      config.LocalhostFilePath = localhostFilePath;
+      var factory = new SplitFactory("localhost", config);
+      var sdk = factory.Client();
     }
   }
 }
